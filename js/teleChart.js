e8c728b948158bddce105e2d013b2a73132e99e9
Valentin Gulyaev <vgulaev@yandex.ru>
2019-03-12 18:03:12 +0500

    Up

--- js/teleChart.js
+++ js/teleChart.js
@@ -58,9 +58,39 @@ class TeleChart {
     return element;
   }
 
+  makeWellStructuredData() {
+    this.data.raw.columns.forEach((element, index) => {
+      this.data.nameByIndex[element[0]] = index;
+    });
+
+    for (const [name, _type] of Object.entries(this.data.raw.types)) {
+      let a = this.data.raw.columns[this.data.nameByIndex[name]];
+      if ('x' == _type) {
+        for (let i = 1; i < a.length - 1; i++) {
+          this.data.x.push(new Date(a[i]));
+        }
+      } else if ('line' == _type) {
+        this.data.y[name] = {
+          coord: []
+        };
+        for (let i = 1; i < a.length - 1; i++) {
+          this.data.y[name].coord.push(a[i]);
+        }
+        this.data.y[name].max = Math.max(...this.data.y[name].coord);
+        this.data.y[name].min = Math.min(...this.data.y[name].coord);
+      }
+    }
+  }
+
   constructor(tagID, data, options = {}) {
     this.svgRoot = document.getElementById(tagID);
-    this.data = data;
+    this.data = {
+      raw: data,
+      x: [],
+      y: {},
+      nameByIndex: {}
+    };
+    this.makeWellStructuredData();
     this.width = this.svgRoot.width.animVal.value;
     this.height = this.svgRoot.height.animVal.value;
     if (options.panelHeight < 1) {
@@ -74,46 +104,41 @@ class TeleChart {
   }
 
   createHeader() {
-    let g = TeleChart.createSVG('g');
-    g.setAttributeNS(null, 'id', 'header');
+  }
 
-    let x = 10;
-    Object.keys(this.data.names).forEach( key => {
-      let text = TeleChart.text(key, {'id': key, 'x': x, 'y': this.width - 10, 'onclick': `alert("${key}");`});
-      x += 25;
-      g.append(text)
+  calcLineCoord(fromX, fromY, dx, dy, data) {
+
+  }
+
+  createMiniMap() {
+    let a = [];
+    let xLength = this.data.x.length;
+    Object.keys(this.data.raw.names).forEach(element => {
+      a.push(this.data.y[element].min);
+      a.push(this.data.y[element].max);
     });
-    this.svgRoot.append(g);
-
-    // g.setAttributeNS(null, 'transform', 'translate(100, -800)');
-    g.setAttributeNS(null, 'cursor', 'col-resize');
-    // console.log(text.getBBox());
-    // console.log('Hello');
-    // this.data.names.forEach(el => {
-
-    // })
+    let miny = Math.min(...a);
+    let maxy = Math.max(...a);
+    for (let i = 0; i < xLength; i++) {
+      console.log(this.data.x[i]);
+    }
+    let path = TeleChart.path({'d': 'M 10,10 L 20,30 40,60 100,200 400,100 0,300 400,400', 'stroke-width': 2, 'stroke': 'black', 'fill': 'none'});
+    this.svgRoot.append(path);
   }
 
   render() {
-    let dy = 30;
-    let element = TeleChart.line(0, 0, this.width, this.height - this.heightPanel - dy, {'stroke-width': 2, 'stroke': 'black'});
+    let element = TeleChart.line(0, 0, this.width, this.height - this.heightPanel, {'stroke-width': 2, 'stroke': 'black'});
     this.svgRoot.append(element);
-    element = TeleChart.line(0, this.height - this.heightPanel - dy, this.width, 0, {'stroke-width': 2, 'stroke': 'black'});
+    element = TeleChart.line(0, this.height - this.heightPanel, this.width, 0, {'stroke-width': 2, 'stroke': 'black'});
     this.svgRoot.append(element);
-    element = TeleChart.rect(1, this.height - this.heightPanel - dy, this.width - 2, this.heightPanel, {
+    element = TeleChart.rect(1, this.height - this.heightPanel, this.width - 2, this.heightPanel, {
       'stroke-width': 2,
       'stroke': 'black',
       'fill': 'none',
       'cursor': 'col-resize'
     });
     this.svgRoot.append(element);
-    element = TeleChart.rect(1, this.height - dy, this.width - 2, dy, {
-      'stroke-width': 2,
-      'stroke': 'red',
-      'fill': 'none',
-      'cursor': 'col-resize'
-    });
-    this.svgRoot.append(element);
     this.createHeader();
+    this.createMiniMap();
   }
 }
