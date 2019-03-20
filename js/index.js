c5a5e98e0b7a824b212f5ce54f0f267c73eaa1c9
Valentin Gulyaev <vgulaev@yandex.ru>
2019-03-20 23:49:11 +0500

    Up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', async function( event ) {
   let data = chartData[6];
-  let chart = new TeleChart('anyChart', data, {
+  let chart = new TeleChart('chart0', data, {
+      width: '500px',
+      height: '200px',
+      widthToPage: true,
+      heightPanel: '100px',
+      animationDuration: 1000
+    });
+  let chart1 = new TeleChart('chart1', chartData[2], {
       width: '500px',
       height: '200px',
       widthToPage: true,
