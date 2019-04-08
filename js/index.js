0fbc5a3783cdf17cb9419974934f00d21343b6db
Valentin Gulyaev <vgulaev@yandex.ru>
2019-04-08 12:54:18 +0500

    test

--- js/index.js
+++ js/index.js
@@ -1,15 +1,30 @@
 function drawCharts() {
   craths = [];
   let dur = parseInt(document.getElementById('animationDuration').value);
-  document.querySelectorAll('.chart').forEach((el, index) =>
-    craths.push(new TeleChart(el.id, chartData[index], {
-      width: '500px',
-      height: '200px',
-      widthToPage: document.getElementById('widthToPage').checked,
-      heightPanel: '100px',
-      animationDuration: dur
-    }))
-    );
+  // document.querySelectorAll('.chart').forEach((el, index) =>
+  //   craths.push(new TeleChart(el.id, chartData[index], {
+  //     width: '500px',
+  //     height: '200px',
+  //     widthToPage: document.getElementById('widthToPage').checked,
+  //     heightPanel: '100px',
+  //     animationDuration: dur
+  //   }))
+  //   );
+  new TeleChart('chart0', chartData[2], {
+        width: '500px',
+        height: '200px',
+        widthToPage: document.getElementById('widthToPage').checked,
+        heightPanel: '100px',
+        animationDuration: dur
+      });
+
+  new TeleChart20('chart1', chartData[2], {
+        width: '500px',
+        height: '200px',
+        widthToPage: document.getElementById('widthToPage').checked,
+        heightPanel: '100px',
+        animationDuration: dur
+      });
 }
 
 window.addEventListener('load', async function( event ) {
---
+++ js/teleChart-2.0.js
@@ -0,0 +1,105 @@
+class TeleChart20 {
+  static get xmlns() {
+    return "http://www.w3.org/2000/svg";
+  }
+
+  static createSVG(tag) {
+    return document.createElementNS(TeleChart.xmlns, tag);
+  }
+
+  static setAttribute(element, atts) {
+    Object.keys(atts).map(key => {
+      element.setAttributeNS(null, key, atts[key]);
+    });
+  }
+
+  static rect(x, y, width, height, options = {}) {
+    let element = TeleChart.createSVG('rect');
+    TeleChart.setAttribute(element, Object.assign({'x': x, 'y': y, 'width': width, 'height': height}, options));
+    return element;
+  }
+
+  static path(options = {}) {
+    let element = TeleChart.createSVG('path');
+    TeleChart.setAttribute(element, options);
+    return element;
+  }
+
+  static circle(cx, cy, r, options = {}) {
+    let element = TeleChart.createSVG('circle');
+    TeleChart.setAttribute(element, Object.assign({'cx': cx, 'cy': cy, 'r': r}, options));
+    return element;
+  }
+
+  constructor(tagID, data, options = {}) {
+    let width = options['width'];
+
+    if (true == options['widthToPage']) {
+      width = document.body.clientWidth - 10 + 'px';
+    }
+
+    this.divRoot = document.getElementById(tagID);
+    this.divRoot.innerHTML = '';
+    this.divRoot.style.width = width;
+
+    this.svgRoot = TeleChart.createSVG('svg');
+    TeleChart.setAttribute(this.svgRoot, {height: options['height'], width: width});
+
+    this.divRoot.append(this.svgRoot);
+
+    this.width = this.svgRoot.width.animVal.value;
+    this.animationStack = new Set();
+    this.render();
+  }
+
+  animationStep() {
+    this.animationTime = performance.now();
+    let callNextStep = false;
+    if (this.animationStack.size > 0) {
+      callNextStep = true;
+      for (let [key, value] of this.animationStack.entries()) {
+        let cont = key.next();
+        if (undefined == cont.value) this.animationStack.delete(key);
+      }
+    }
+
+    if (callNextStep) {
+      requestAnimationFrame(() => this.animationStep())
+    };
+  }
+
+  *moveCircle() {
+    let startTime = performance.now();
+    let x = this.c.getBBox().x;
+    console.log(this.width);
+    yield 'start';
+    while (true) {
+    // for (let i = 0; i < 200; i++) {
+      let dx = (this.animationTime - startTime) / 1000 * 150;
+      // console.log(dx);
+      this.c.setAttributeNS(null, 'cx', (x + dx) % this.width);
+      yield true;
+    }
+  }
+
+  doAnimation(animation) {
+    if (animation != undefined) {
+      this.animationStack.add(animation);
+      animation.next();
+    }
+
+    requestAnimationFrame(() => this.animationStep());
+  }
+
+
+  render() {
+    this.c = TeleChart20.circle(100, 100, 20, {'fill': '#E8AF14'});
+    this.svgRoot.append(this.c);
+
+    let a = this.moveCircle();
+    this.doAnimation(a);
+    console.log('Hello');
+  }
+}
+
+// alert('TeleChart20 here');
